d4172c1a1e423cc69ed42f6034f3617745d6144d
Phuong Ngo <ngohoai.phuong@gmail.com>
2020-09-21 09:52:23 +0700

    use bootstrap, jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,13 @@
-require("@rails/ujs").start()
+import 'bootstrap'
+
+require("rails-ujs-confirm").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
 import "controllers"
 
+import '../stylesheets/application'
 import I18n from 'shared/locale.js.erb'
 
 $(document).on('turbolinks:load', function () {
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,17 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 
+const webpack = require("webpack");
+
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"],
+    Rails: ['rails-ujs-confirm']
+  })
+);
+
 environment.loaders.prepend('erb', erb)
 module.exports = environment
